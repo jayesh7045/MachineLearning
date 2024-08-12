42341e35471746fd9410c202166c7f1055b5884c
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-08-12 15:41:20 +0530

    Efficient Palindrom function add

--- MachineLearning/Section5/Palindrome.py
+++ MachineLearning/Section5/Palindrome.py
@@ -1,4 +1,4 @@
-stringtemp = "EAAEA";
+stringtemp = "A man a plan a canal panama";
 def findPalindrom(stringtemp): 
     i = 0;
     j = len(stringtemp)-1;
@@ -13,3 +13,11 @@ if findPalindrom(stringtemp):
     print("Palindrome");
 else:
     print("NA")
+    
+def EffPalin(stringtemp):
+    stringtemp = stringtemp.lower().replace(" ", "");
+    return stringtemp == stringtemp[::-1]
+if EffPalin(stringtemp):
+    print("Palindrome Yes");
+else:
+    print("No")
