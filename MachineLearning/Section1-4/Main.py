68b8aa1b9ab3269a188dfb4bfa7a9dbc145e8215
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-08-12 15:24:54 +0530

    Env Setup using  conda

---
+++ MachineLearning/Section1-4/Main.py
@@ -0,0 +1,16 @@
+a = 1+2+3+4+5+6+7+\
+    9+10+11
+print(a)
+
+b=10;c=34;g=100
+print(b,c,g)
+
+
+intvalue = 90
+print(type(intvalue))
+ji = str(intvalue)
+print(ji)
+print(type(ji))
+
+mixed_list = [1, 2, 3, 4, "Hello World"]
+print(mixed_list)
